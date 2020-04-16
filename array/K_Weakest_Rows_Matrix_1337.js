a9546898781f9231d203b7dea46b7e09a92e7a72
Lam Gia Hung <lamgiahung112@gmail.com>
2020-04-16 09:26:35 +0700

    add this baby

--- array/K_Weakest_Rows_Matrix_1337.js
+++ array/K_Weakest_Rows_Matrix_1337.js
@@ -1,5 +1,9 @@
 // ones = soldiers
 // zeros = civilians
 let kWeakestRows = function(matrix, k) {
-    
+    return matrix
+                .map((row,index) => [index, row.filter(n => n ===1).length]) // [index, soldier count]
+                .sort(([i1,c1],[i2,c2]) => c1-c2 || i1-i2) // sort by soldier count first
+                .map(([index]) => index)    // remove soldier count
+                .slice(0,k);    // slice
 };
